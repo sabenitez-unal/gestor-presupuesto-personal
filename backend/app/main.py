100b2b21b4a4b34f9f38a59b969860eca83b3612
sabenitez-unal <sabenitez@unal.edu.co>
2026-08-22 21:17:44 -0500

    learning: aprendizaje sobre path parameters

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app =  FastAPI()
 
@@ -6,6 +7,26 @@ app =  FastAPI()
 def read_root():
     return {"mensaje" : "Guten Morgen!"}
 
+# Path parameters
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
     return {"ItemID" : item_id}
+
+# Importancia del orden de rutas: primero, las fijas y luego, variables
+@app.get("/users/me")
+def read_current_user():
+    return {"Usuario" : "Samuel"}
+
+@app.get("/users/{user_id}")
+def read_user(user_id: str):
+    return {"ItemID" : user_id}
+
+# Tipos restringidos (enums)
+class Modelo(str, Enum):
+    modelo_a = "Modelo A"
+    modelo_b = "Modelo B"
+    modelo_c = "Modelo C"
+
+@app.get("/modelos/{nombre_modelo}")
+def obtener_modelo(nombre_modelo: Modelo):
+    return {"modelo" : nombre_modelo}
