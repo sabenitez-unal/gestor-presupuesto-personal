3fd4baab6f0a409c9be30328d1c840773d5b30a7
sabenitez-unal <sabenitez@unal.edu.co>
2026-08-26 15:45:07 -0500

    fix: comprobacion de duplicados en creacion de tareas

--- backend/app/routers/tareas.py
+++ backend/app/routers/tareas.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, HTTPException
 from schemas.tarea import TareaResponse, Tarea
 
 router = APIRouter(
@@ -11,8 +11,16 @@ tareas: list[TareaResponse] = []
 counter = 0
 
 # Creacion de tarea
-@router.post("/tareas", response_model=TareaResponse)
+@router.post("/tareas", response_model=TareaResponse, status_code=status.HTTP_201_CREATED)
 def crear_tarea(tarea: Tarea):
+    # Comprobar duplicados
+    for t in tareas:
+        if t.titulo == tarea.titulo: 
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"La tarea '{tarea.titulo}' ya existe."
+            )
+
     global counter
     nueva_tarea = TareaResponse(id=counter, **tarea.model_dump())
     tareas.append(nueva_tarea)
